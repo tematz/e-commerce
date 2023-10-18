5c5ac2c81cbc63b98993a46d4166af738584f1b8
Matheus Vieira <matvno@gmail.com>
2023-10-18 16:20:46 -0300

    feat: add auth handler to header components

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
-import { prismaClient } from "@/lib/prisma";
+import { prismaClient } from "./prisma";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
