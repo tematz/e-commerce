37033444d1065df6822db3ffd9af32aa32c91f18
Matheus Vieira <matvno@gmail.com>
2023-10-19 16:09:36 -0300

    feat: add mouses section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -20,6 +20,14 @@ export default async function Home() {
       },
     },
   });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
   return (
     <div className="flex flex-col gap-8">
       <PromoBanner
@@ -49,6 +57,11 @@ export default async function Home() {
           src="/banner-home-03.png"
           alt="Até 55% de desconto esse mouses!"
         />
+      </div>
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   );
