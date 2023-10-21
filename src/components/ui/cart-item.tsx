5ea427aba1f72d9c9a09dc4d6a760cb637f611e5
Matheus Vieira <matvno@gmail.com>
2023-10-21 18:22:34 -0300

    feat: ensure product quantify can be increased from cart

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -9,10 +9,15 @@ interface CartItemProps {
 }
 
 const CartItem = ({ product }: CartItemProps) => {
-  const { decreaseProductQuantity } = useContext(CartContext);
+  const { decreaseProductQuantity, increaseProductQuantity } =
+    useContext(CartContext);
 
   const handleDecreaseProductQuantityClick = () => {
     decreaseProductQuantity(product.id);
+  };
+
+  const handleIncreaseProductQuantifyClick = () => {
+    increaseProductQuantity(product.id);
   };
 
   return (
@@ -53,7 +58,12 @@ const CartItem = ({ product }: CartItemProps) => {
               <ArrowLeftIcon size={16} />
             </Button>
             <span className="text-xs">{product.quantity}</span>
-            <Button size="icon" variant="outline" className="h-8 w-8">
+            <Button
+              size="icon"
+              variant="outline"
+              className="h-8 w-8"
+              onClick={handleIncreaseProductQuantifyClick}
+            >
               <ArrowRightIcon size={16} />
             </Button>
           </div>
