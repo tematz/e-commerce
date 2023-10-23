bb761ecaf1677fe024507d275424bac943bc6101
Matheus Vieira <matvno@gmail.com>
2023-10-23 16:38:06 -0300

    feat: add HOST_URL

--- env.d.ts
+++ env.d.ts
@@ -6,5 +6,7 @@ declare namespace NodeJS {
     STRIPE_SECRET_KEY: string;
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
     STRIPE_WEBHOOK_SECRET_KEY: string;
+    NEXTAUTH_SECRET: string;
+    HOST_URL: string;
   }
 }
